2fbc18c620a133343e6181cc51903cefb5d87a50
Mihir <mihir1003@gmail.com>
2023-03-03 17:33:36 -0800

    lab 1 completion and checking: ** git push

--- lab1_part2_complete.py
+++ lab1_part2_complete.py
@@ -9,6 +9,8 @@ if __name__ == '__main__':
     print(run_command(container_dict["client"], "sudo sysctl -w net.ipv4.ip_forward=1"))
     print(run_command(container_dict["metasploitable"], "sudo sysctl -w net.ipv4.ip_forward=1"))
     print(run_command(container_dict["router"], "sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE"))
+    print(run_command(container_dict["router"], "sudo iptables --list "))
     print(run_command(container_dict["testing"],"sudo iptables -t nat -A POSTROUTING -o enp0s8 -j MASQUERADE"))
+    print(run_command(container_dict["testing"], "sudo iptables --list "))
 
 
